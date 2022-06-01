131b27eb75960686176843a3a0f080b1f24da48f
Rill <rillw@icloud.com>
2022-06-01 15:12:47 -0500

    Nikola auto commit.
    Nikola version: 8.2.2

--- conf.py
+++ conf.py
@@ -21,6 +21,8 @@ BLOG_TITLE = "Star Base Cafe"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://rillw.github.io/rillw/"
+
+
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://rillw.github.io/rillw/"
